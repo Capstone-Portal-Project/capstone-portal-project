4e434adbb7722f8ab920cf1d850cc199ad6d31e2
Sankalp Patil <patilsa@oregonstate.edu>
2025-01-13 23:01:37 -0800

    db connected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/4033f3b2-0fc9-4a6e-95be-37009f50e078-7qajqq.png",
@@ -6,17 +7,23 @@ const mockUrls = [
   "https://utfs.io/f/4709ed08-e620-4c42-9d65-94af53585014-lf3twj.png",
   "https://utfs.io/f/c494ae05-dd1a-459a-b09d-bbe37a140853-qikpqu.1.png",
 ];
+
 const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
           </div>
         ))}
