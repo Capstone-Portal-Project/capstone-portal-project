30fbb1562bb4545e0a931375a91b53cb206e6699
mfaks <muhammad.faks@gmail.com>
2025-01-27 19:06:09 -0800

    feat: removed references to uploadThing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,6 @@ import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-
-async function Projects() {
-  const projects = await db.query.projects.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
-  return (
-    <div className="flex flex-wrap gap-4">
-      {projects.map((project) => (
-        <div key={project.id} className="flex w-48 flex-col">
-          <img src={project.url} />
-          <div>{project.name}</div>
-        </div>
-      ))}
-    </div>
-  );
-}
-
 export default async function HomePage() {
   return (
     <main className="">
@@ -29,7 +12,9 @@ export default async function HomePage() {
         </div>
       </SignedOut>
       <SignedIn>
-        <Projects />
+        <div className="h-full w-full text-center text-2xl">
+          Welcome to the Capstone Project Portal
+        </div>
       </SignedIn>
     </main>
   );
