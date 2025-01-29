540a20ec8e3632894ee157935a28862584983a9c
Sankalp Patil <patilsa@oregonstate.edu>
2025-01-29 15:23:51 -0800

    landing page and navbar

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,22 +1,68 @@
 "use client";
 
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle
+} from "../../components/ui/navigation-menu"
+import { Button } from "../../components/ui/button"
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
+import Link from "next/link"
+import Image from 'next/image'
 
 export function TopNav() {
   const router = useRouter();
   return (
-    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div></div>
-
-      <div className="flex flex-row">
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+    <nav className="flex w-full items-center justify-between p-2 text-primary-foreground bg-gray-800">
+      <div className="flex items-center gap-2">
+        <Image
+          src="/oregonStateLogo.png"
+          width={60}
+          height={60}
+          alt="Oregon State Logo"
+        />
+        <h2 className="text-2xl font-semibold">Engineering Capstone</h2>
       </div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/browse" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Browse Current Projects
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/showcase" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Showcase
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/submit" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Submit a Project
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <SignedOut>
+              <SignInButton>
+                <Button className="rounded-md bg-gray-800 px-4 py-2 text-md font-light transition-colors hover:bg-gray-700 hover:text-accent-background focus:bg-accent focus:text-accent-foreground">
+                  Sign In
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </nav>
   );
 }
