d27ba0ee018173cfe208bef7c829c001d5fc897d
Sankalp Patil <patilsa@oregonstate.edu>
2025-02-03 19:13:25 -0800

    added instructor links to navbar

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,8 +2,9 @@ import { auth } from '@clerk/nextjs/server'
 import React from 'react'
 
 export default function Admin() {
-  auth.protect({ role: 'org:admin' })
     
+  auth.protect();    
+
   return (
     <div>Admin</div>
   )
--- src/app/instructor/page.tsx
+++ src/app/instructor/page.tsx
@@ -2,7 +2,6 @@ import { auth } from '@clerk/nextjs/server'
 import React from 'react'
 
 export default function Instructor() {
-  auth.protect({ role: 'org:instructor' })
   
   return (
     <div>Instructor</div>
