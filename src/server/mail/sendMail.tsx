28e81f92c4c3083222019c65a8299dc9541b18be
Justin Pham <phamjus@oregonstate.edu>
2025-02-11 22:05:58 -0600

    Created API to send various projectLog emails

--- src/server/mail/sendMail.tsx
+++ src/server/mail/sendMail.tsx
@@ -2,13 +2,75 @@
 import React from "react";
 import { render } from "@react-email/render";
 import { sendEmail } from "./mailer";
-import { EmailTemplate } from "./EmailTemplate";
+import { EmailTemplate } from "./email_templates/EmailTemplate";
 
-const recipientEmail = "phamjus@oregonstate.edu";
-const name = "John Doe";
+export enum projectLogTypes {
+    SUBMISSION,
+    DEFERMENT,
+    APPROVAL,
+    PARTNER_MESSAGE,
+    INSTRUCTOR_ADMIN_MESSAGE,
+    COURSE_TRANSFER
+}
 
-const emailHtml = await render(<EmailTemplate name={name} />, {
-    pretty: true,
-});
+export async function sendLogEmail(logType: projectLogTypes, recipientEmail: string, cc: Array<string>, recipientName: string, projectName: string, projectLink: string) {
 
-await sendEmail(recipientEmail, "Welcome!", emailHtml);
+    switch (logType) {
+        case projectLogTypes.SUBMISSION:
+            await sendEmail(recipientEmail, cc, await 
+                render(<EmailTemplate
+                    projectLogType={projectLogTypes.SUBMISSION}
+                    recipientName={recipientName} 
+                    projectName={projectName}
+                    projectLink={projectLink}
+                />));
+            break;
+        case projectLogTypes.DEFERMENT:
+            await sendEmail(recipientEmail, cc, await 
+                render(<EmailTemplate
+                    projectLogType={projectLogTypes.DEFERMENT}
+                    recipientName={recipientName} 
+                    projectName={projectName}
+                    projectLink={projectLink}
+                />));
+            break;
+        case projectLogTypes.APPROVAL:
+            await sendEmail(recipientEmail, cc, await 
+                render(<EmailTemplate
+                    projectLogType={projectLogTypes.APPROVAL}
+                    recipientName={recipientName} 
+                    projectName={projectName}
+                    projectLink={projectLink}
+                />));
+            break;
+        case projectLogTypes.PARTNER_MESSAGE:
+            await sendEmail(recipientEmail, cc, await 
+                render(<EmailTemplate
+                    projectLogType={projectLogTypes.PARTNER_MESSAGE}
+                    recipientName={recipientName} 
+                    projectName={projectName}
+                    projectLink={projectLink}
+                />));
+            break;
+        case projectLogTypes.INSTRUCTOR_ADMIN_MESSAGE:
+            await sendEmail(recipientEmail, cc, await 
+                render(<EmailTemplate
+                    projectLogType={projectLogTypes.INSTRUCTOR_ADMIN_MESSAGE}
+                    recipientName={recipientName} 
+                    projectName={projectName}
+                    projectLink={projectLink}
+                />));
+            break;
+        case projectLogTypes.COURSE_TRANSFER:
+            await sendEmail(recipientEmail, cc, await 
+                render(<EmailTemplate
+                    projectLogType={projectLogTypes.COURSE_TRANSFER}
+                    recipientName={recipientName} 
+                    projectName={projectName}
+                    projectLink={projectLink}
+                />));
+            break;
+    }
+
+    return;
+}
