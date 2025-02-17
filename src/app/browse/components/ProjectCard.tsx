b4a6fe075d5b153a2e2d283da6f2979c351f75b6
Han Zhu <hzhu8127@gmail.com>
2025-02-17 08:55:28 -0800

    refactor/style: refactor save button as separate component, and style for md&2xl

--- src/app/browse/components/ProjectCard.tsx
+++ src/app/browse/components/ProjectCard.tsx
@@ -1,7 +1,6 @@
-import { Pin } from "lucide-react";
 import { Badge } from "~/components/ui/badge";
-import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
+import PinButton from "./PinButton";
 
 
 type ProjectCardProps = {
@@ -15,23 +14,24 @@ const ProjectCard = (props: ProjectCardProps) => {
     const CHAR_LIMIT = 110;
     const { imgUrl, title, description, tags } = props;
 
+    const clickHandler = () => {
+    }
+
     return (
-        <Card className="bg-[#f7f5f5]">
-        <CardHeader className="flex-row pt-3 pb-1 justify-end">
-            <Button className="bg-[#f7f5f5] text-[#423e3c] hover:bg-[#e9e5e4] h-8 w-8 [&_svg]:size-5">
-            <Pin />
-            </Button>
-        </CardHeader>
-        <CardContent className="space-y-3">
-            <img src={imgUrl} />
-            <CardTitle>{ title }</CardTitle>
-            <CardDescription className="text-sm">{
-            description?.slice(0, description.indexOf(' ', CHAR_LIMIT)) + '...' 
-            }</CardDescription>
-            <div className="flex flex-row gap-2 flex-wrap">
-            {tags?.map((tag, index) => <Badge className="rounded-sm" key={index}>{tag}</Badge>)}
-            </div>
-        </CardContent>
+        <Card className="bg-[#f7f5f5] shadow-md">
+            <CardHeader className="flex-row pt-3 pb-1 justify-end">
+                <PinButton onClick={clickHandler} />
+            </CardHeader>
+            <CardContent className="space-y-3">
+                <img src={imgUrl} className="" />
+                <CardTitle className="">{ title }</CardTitle>
+                <CardDescription className="text-sm">{
+                description?.slice(0, description.indexOf(' ', CHAR_LIMIT)) + '...' 
+                }</CardDescription>
+                <div className="flex flex-row gap-2 flex-wrap">
+                {tags?.map((tag, index) => <Badge className="rounded-sm" key={index}>{tag}</Badge>)}
+                </div>
+            </CardContent>
         </Card>
     );
 }
