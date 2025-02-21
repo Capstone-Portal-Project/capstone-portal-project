be26c1e6e1752ac39373cfc97338bcb00535f99f
Justin Pham <phamjus@oregonstate.edu>
2025-02-21 00:03:08 -0600

    Seperated Page Content from Form + Updated Form

--- src/app/project/[project_id]/page.tsx
+++ src/app/project/[project_id]/page.tsx
@@ -1,18 +1,45 @@
 import ProjectPageClient from "./projectPage";
-import { getDummyData } from "./utils/getDummyData";
 import getProjectProps from "./utils/getProjectProps";
-
-interface BrowseProjectsParams {
-  params: Promise<{ project_id: string }>;
-}
+import type { ProjectSchema } from "~/app/_components/editprojectpage";
+import { getProjectById } from "~/server/api/routers/project";
 
 export default async function ProjectPage({ params }: { params: Promise<{project_id: string}> }) {
 
   const projectId = (await params).project_id;
-  const projectDetails = await getProjectProps(Number(projectId));
+  const pageContent = await getProjectProps(Number(projectId));
+  const projectResponse = await getProjectById(Number(projectId));
 
-  console.log(projectDetails);
+  if (projectResponse?.project) {
+    const projectDetails : ProjectSchema = {
+      projectId: Number(projectId),
+      programsId: projectResponse.project.programsId,
+      projectTitle: projectResponse.project.projectTitle,
+      appImage: projectResponse.project.appImage,
+      appVideo: projectResponse.project.appVideo,
+      appOrganization: projectResponse.project.appOrganization,
+      appDescription: projectResponse.project.appDescription,
+      appObjectives: projectResponse.project.appObjectives,
+      appMotivations: projectResponse.project.appMotivations,
+      appMinQualifications: projectResponse.project.appMinQualifications,
+      appPrefQualifications: projectResponse.project.appPrefQualifications,
+      showcaseDescription: projectResponse.project.showcaseDescription,
+      showcaseImage: projectResponse.project.showcaseImage,
+      showcaseVideo: projectResponse.project.showcaseVideo,
+      isShowcasePublished: projectResponse.project.isShowcasePublished,
+      sequenceId: projectResponse.project.sequenceId,
+      sequenceReport: projectResponse.project.sequenceReport,
+      projectGithubLink: projectResponse.project.projectGithubLink
+    }
 
-  return <ProjectPageClient project={projectDetails} />;
+    return <ProjectPageClient 
+      pageContent={pageContent} 
+      project={projectDetails}
+    />;
+  }
+
+  if (!projectResponse?.project) {
+    return <div>Project not found</div>;
+  }
+
 
 }
--- src/app/project/[project_id]/projectPage.tsx
+++ src/app/project/[project_id]/projectPage.tsx
@@ -1,61 +1,33 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import Detail from "./components/detail";
-import Hero from "./components/hero";
-import Content from "./components/content";
+import { useState } from "react";
+import ProjectContent from "./components/projectContent";
 import type { ProjectProps } from "./utils/getDummyData";
 import ProjectEditSidebarPopout from "~/app/_components/editprojectpage";
+import type { ProjectSchema } from "~/app/_components/editprojectpage";
 import { Button } from "src/components/ui/button";
 import { SquarePenIcon, EyeOffIcon } from "lucide-react";
 
-
-interface ProjectPageClientProps {
-  project: ProjectProps;
+export interface ProjectPageClientProps {
+  pageContent: ProjectProps;
+  project: ProjectSchema;
 }
 
-export default function ProjectPageClient({ project: initialProject }: ProjectPageClientProps) {
-  const [project, setProject] = useState<ProjectProps>(initialProject);
+export default function ProjectPageClient({ pageContent: initialProject, project: projectDetails }: ProjectPageClientProps) {
+  const [pageContent, setPageContent] = useState<ProjectProps>(initialProject);
   const [isSidebarVisible, setSidebarVisible] = useState(true);
 
-  if (!project) return <div>Loading...</div>;
+  if (!pageContent) return <div>Loading...</div>;
 
   return (
     <>
       {isSidebarVisible && (
         <ProjectEditSidebarPopout
-          className="border-4 border-gray-300 rounded-lg"
-          cp_id={0}
-          course_id={[]}
-          cp_title={""}
-          cp_date_created={""}
-          cp_date_updated={""}
-          cp_archived={false}
-          {...project}
+          project={projectDetails}
         />
       )}
 
-      <main className="w-full h-full bg-white-off grid grid-rows-[100vh]">
-        <Hero {...project.hero} />
-        <div className="px-16 py-4">
-          <div className="grid grid-cols-2">
-            <Content {...project.content} />
-            <div className="flex justify-self-end">
-              <span className="shrink-0 bg-border h-full w-[1px] block bg-copy"></span>
-              <Detail
-                {...{
-                  ...project.infoCard,
-                  details: {
-                    ...project.infoCard.details,
-                    "Project Status": project.infoCard.details["Project Status"] ?? false,
-                  },
-                  keywords: project.infoCard.keywords.map(keyword => keyword.tag ?? ""),
-                }}
-              />
-            </div>
-          </div>
-        </div>
-      </main>
+      <ProjectContent {...pageContent} />
 
       <Button
         onClick={() => setSidebarVisible(prev => !prev)}
