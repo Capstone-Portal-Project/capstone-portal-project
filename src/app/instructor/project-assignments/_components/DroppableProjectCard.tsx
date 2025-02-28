8ac0f73e5a5503d367c0f7a2e9e367431073bf2a
Sankalp Patil <patilsa@oregonstate.edu>
2025-02-28 11:52:53 -0800

    drag n drop basics work

--- src/app/instructor/project-assignments/_components/DroppableProjectCard.tsx
+++ src/app/instructor/project-assignments/_components/DroppableProjectCard.tsx
@@ -1,10 +1,12 @@
-import { Badge } from "~/components/ui/badge";
+"use client";
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
 import Link from "next/link";
-import { useSortable } from "@dnd-kit/sortable";
+import { useDroppable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
 import { DataTableUser, columns } from "../columns";
 import UserCard from "./UserCard";
+import { useMemo } from "react";
 
 
 type ProjectCardProps = {
@@ -19,33 +21,35 @@ type ProjectCardProps = {
 const DroppableProjectCard = (props: ProjectCardProps) => {
     const CHAR_LIMIT = 150;
     const { imgUrl, title, description, tags, projectId, users } = props;
-    const { setNodeRef, attributes, listeners, transform, transition } = useSortable({
-        id: projectId!.toString(),
+    const { setNodeRef } = useDroppable({
+        id: projectId!,
         data: {
             type: "project",
             projectId,
         }
     })
-    const style = {
-        transition,
-        transform: CSS.Transform.toString(transform),
-    }
+    // const style = {
+    //     transition,
+    //     transform: CSS.Transform.toString(transform),
+    // }
+
+    const userIds = useMemo(() => users.map((user) => user.id), [users]);
 
     return (
-        <div ref={setNodeRef} style={style} className="h-[250px] max-h-[250px]">
+        <div ref={setNodeRef} className="h-[250px] max-h-[250px]">
             <Card className="h-full flex flex-col">
-                <CardHeader {...attributes} {...listeners} className="pt-3 pb-1 hover:underline">
+                <CardHeader  className="pt-3 pb-1 hover:underline">
                     <CardTitle className="text-xl font-semibold line-clamp-2">
                         {title}
                     </CardTitle>
                 </CardHeader>
                 <CardContent className="m-4 bg-slate-200 rounded-md flex-grow">
-                    {/* map the user data into a column here */}
                     <div className="m-2 flex flex-col gap-2">
-                        {users.map((user) => (
-                            <UserCard key={user.id} user={user} />
-                            
-                        ))}
+                        {/* <SortableContext items={userIds}> */}
+                            {users.map((user) => (
+                                <UserCard key={user.id} user={user} />
+                            ))}
+                        {/* </SortableContext> */}
                     </div>
                 </CardContent>
             </Card>
--- src/app/instructor/project-assignments/page.tsx
+++ src/app/instructor/project-assignments/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useMemo, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DataTableUser, columns } from "./columns";
 import { DataTable } from "./data-table";
 import { useUser } from "@clerk/clerk-react";
@@ -13,8 +13,7 @@ import {
     ResizablePanelGroup,
   } from "~/components/ui/resizable"
 
-import { DndContext, DragStartEvent } from "@dnd-kit/core";
-import { SortableContext } from "@dnd-kit/sortable";
+import { DndContext, DragStartEvent, DragEndEvent } from "@dnd-kit/core";
 import { users } from "~/server/db/schema";
 
 type Project = {
@@ -31,7 +30,6 @@ export default function ProjectAssignments() {
   const [userData, setUserData] = useState<DataTableUser[]>([]);
   const [projects, setProjects] = useState<Project[]>([]);
   const [loading, setLoading] = useState(true);
-  const projectsId = useMemo(() => projects.map((project) => project.projectId), [projects]);
 
   // On page load, fetch students and projects for the program (course)
   useEffect(() => {
@@ -95,55 +93,74 @@ export default function ProjectAssignments() {
 
 
   function onDragStart(event: DragStartEvent) {
-    console.log(event);
+    const { active } = event;
+    console.log(`User ${active.id} started dragging`);
   }
+
+  function onDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+    if (!over) return;
+
+    console.log(`User ${active.id} dragged to project ${over.id}`);
+
+    const userId = active.data.current!.userId as number;
+    const newProject = over.data.current!.projectId as number;
+
+    // Update the user's project ID in the state, then the database
+    setUserData(() =>
+        userData.map((user) => 
+            user.id === userId ? { ...user, projectId: newProject } : user
+        )
+    )
+
+    console.log(userData);
+
+    // TODO BELOW - Update the user's project ID in the database
+  }
+
 
   return (
     <div className="container mx-auto py-10 h-full flex gap-2">
-            <DndContext onDragStart={onDragStart}>
-                {/* Students List (Draggable) */}
-                {/* <DataTable columns={columns} data={userData} /> */}
-                {/* map the user data into a column here */}
-                <div>
-                    <h3 className="text-2xl font-semibold mx-4 mb-4">
-                        Students
-                    </h3>
-                    <div className="flex flex-col gap-6 px-4">
-                        {userData.map((user) => (
-                            <div key={user.id} className="bg-white rounded-md shadow-md p-4">
-                                <h4 className="text-lg font-semibold">{user.username}</h4>
-                                <p className="text-sm text-gray-500">{user.email}</p>
-                            </div>
-                        ))}
+        {/* Students List (Draggable) */}
+        {/* <DataTable columns={columns} data={userData} /> */}
+        {/* map the user data into a column here */}
+        <div>
+            <h3 className="text-2xl font-semibold mx-4 mb-4">
+                Students
+            </h3>
+            <div className="flex flex-col gap-6 px-4">
+                {userData.map((user) => (
+                    <div key={user.id} className="bg-white rounded-md shadow-md p-4">
+                        <h4 className="text-lg font-semibold">{user.username}</h4>
+                        <p className="text-sm text-gray-500">{user.email}</p>
                     </div>
-                </div>
-                {/* Projects List (Droppable) */}
-                <div>
-                    <h3 className="text-2xl font-semibold mx-4 mb-4">
-                        Projects
-                    </h3>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 px-4">
-                        <SortableContext items={projectsId}>
-                            {projects.map((project) => (
-                                <DroppableProjectCard
-                                key={project.projectId}
-                                projectId={project.projectId}
-                                imgUrl={project.appImage || ''}
-                                title={project.projectTitle}
-                                description={project.appDescription}
-                                tags={[project.appOrganization]}
-                                users={userData.filter(user => user.projectId === project.projectId)}
-                                />
-                            ))}
-                            {projects.length === 0 && (
-                                <div className="col-span-full text-center py-10 text-gray-500">
-                                No active projects available
-                                </div>
-                            )}
-                        </SortableContext>
-                    </div>
-                </div>
-            </DndContext>
+                ))}
+            </div>
+        </div>
+        {/* Projects List (Droppable) */}
+        <div>
+            <h3 className="text-2xl font-semibold mx-4 mb-4">
+                Projects
+            </h3>
+            <div className="flex gap-6 px-4">
+                <DndContext 
+                onDragStart={onDragStart}
+                onDragEnd={onDragEnd}   
+                >
+                    {projects.map((project) => (
+                        <DroppableProjectCard
+                            key={project.projectId}
+                            projectId={project.projectId}
+                            imgUrl={project.appImage || ''}
+                            title={project.projectTitle}
+                            description={project.appDescription}
+                            tags={[project.appOrganization]}
+                            users={userData.filter(user => user.projectId === project.projectId)}
+                        />
+                    ))}
+                </DndContext>
+            </div>
+        </div>
     </div>
   );
 }
